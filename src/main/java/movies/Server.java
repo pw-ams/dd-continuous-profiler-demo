b06debc70218c37fe1b07f81c32e8d10f0cf8ada
pw-ams <pwright@amerisave.com>
2026-05-27 14:55:39 -0400

    Update Server.java

--- src/main/java/movies/Server.java
+++ src/main/java/movies/Server.java
@@ -100,7 +100,8 @@ public class Server {
 		return replyJSON(res, moviesWithCredits);
 	}
 
-	private static synchronized Object statsEndpoint(Request req, Response res) {
+	private static Object statsEndpoint(Request req, Response res) {
+	//private static synchronized Object statsEndpoint(Request req, Response res) {
 		var movies = MOVIES.get().stream();
 		var query = req.queryParamOrDefault("q", req.queryParams("query"));
 
